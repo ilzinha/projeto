055cf87c60351121137f2acd60e4004ae7fd722a
Ilza <ilzammed2@gmail.com>
2019-11-26 01:23:37 -0300

    definindo o banco

---
+++ config/database.js
@@ -0,0 +1,15 @@
+
+module.export: {
+    "dialect": "sqlite",
+    "storage": "banco.sqlite",
+  }
+
+  {
+    "development": {
+      "username": "root",
+      "password": null,
+      "database": "database_development",
+      "host": "127.0.0.1",
+      "dialect": "mysql",
+      "operatorsAliases": false
+    }
